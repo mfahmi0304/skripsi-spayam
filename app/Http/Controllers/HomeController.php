517500ba39d035bec34239f629d4bcc698105b0f
Muhammad Fahmi Nur Aziz <m.fahminuraziz93@gmail.com>
2022-06-23 15:23:21 +0700

    [UPDATE] Dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,16 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $gejala = DB::table('gejalas')->count();
+        $penyakit = DB::table('penyakits')->count();
+        $basis_pengetahuan = DB::table('basis_pengetahuans')->count();
+
+        $data = array(
+            'gejala'     => $gejala,
+            'penyakit'     => $penyakit,
+            'basis_pengetahuan' => $basis_pengetahuan
+        );
+
+        return view('home', compact('data'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Selamat Datang di SIPABANG (Sistem Pakar Diagnosa Penyakit Ayam Bangkok)') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,36 @@
                         </div>
                     @endif
 
-                    {{ __('Selamat datang di Sistem Pakar Diagnosa Penyakit Ayam Bangkok') }}
+                    {{ __('Anda Login Sebagai Admin') }}
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-12 col-sm-8 col-md-4">
+            <div class="info-box">
+                <span class="info-box-icon bg-info elevation-1"><i class="fas fa-thermometer-half"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Total Gejala</span>
+                    <span class="info-box-number">{{ $data['gejala'] }}</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-8 col-md-4">
+            <div class="info-box mb-3">
+                <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-bug"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Total Penyakit</span>
+                    <span class="info-box-number">{{ $data['penyakit'] }}</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-8 col-md-4">
+            <div class="info-box mb-3">
+                <span class="info-box-icon bg-success elevation-1"><i class="fas fa-lightbulb"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Total Aturan</span>
+                    <span class="info-box-number">{{ $data['basis_pengetahuan'] }}</span>
                 </div>
             </div>
         </div>
